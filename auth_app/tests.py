72e2b932c610d2743d63f9933e5caf7768af8410
CalvinH-Dev <hanisch.calvin@gmail.com>
2026-02-03 13:32:17 +0100

    fix: improved readability of Registration & Login Tests

--- auth_app/tests.py
+++ auth_app/tests.py
@@ -23,17 +23,20 @@ class RegistrationTest(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(User.objects.count(), 1)
         self.assertEqual(UserProfile.objects.count(), 1)
-        self.assertEqual(User.objects.get().username, "exampleUsername")
+        self.assertEqual(User.objects.get().username, self.data["username"])
         self.assertEqual(UserProfile.objects.get().user_id, 1)  # type: ignore
-        self.assertEqual(UserProfile.objects.get().type, "customer")
+        self.assertEqual(UserProfile.objects.get().type, self.data["type"])
 
 
 class LoginTest(APITestCase):
     def setUp(self):
+        self.user_data = {
+            "username": "exampleUsername",
+            "password": "examplePassword",
+        }
         self.user = User.objects.create_user(
-            username="exampleUsername",
             email="example@mail.de",
-            password="examplePassword",
+            **self.user_data,
         )
         UserProfile.objects.create(user=self.user, type="customer")
         token_obj = Token.objects.create(user=self.user)
@@ -41,11 +44,7 @@ class LoginTest(APITestCase):
 
     def test_login(self):
         url = reverse("login")
-        data = {
-            "username": "exampleUsername",
-            "password": "examplePassword",
-        }
-        response = self.client.post(url, data, format="json")
+        response = self.client.post(url, self.user_data, format="json")
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data["token"], self.token)  # type: ignore
         self.assertEqual(response.data["username"], "exampleUsername")  # type: ignore
