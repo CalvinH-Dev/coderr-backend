3437418a80f96a495479a3bd289e5321ab0dea85
CalvinH-Dev <hanisch.calvin@gmail.com>
2026-02-11 16:29:24 +0100

    refactor: changed testing of dicts with .pop() and assertion at the end with {} to verify, only the wanted fields are in the response.

--- offers_app/tests.py
+++ offers_app/tests.py
@@ -1,5 +1,3 @@
-import json
-
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.authtoken.admin import User
@@ -149,15 +147,19 @@ class TestOfferPackageViewSet(APITestCaseWithSetup):
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         data = response.json()
-        self.assertEqual(data["user"], self.offer_package_1.user.id)
-        self.assertEqual(data["title"], self.offer_package_1.title)
-        self.assertEqual(data["description"], self.offer_package_1.description)
-        self.assertEqual(data["image"], self.offer_package_1.image)
-        self.assertEqual(data["min_price"], 100)
-        self.assertEqual(data["min_delivery_time"], 5)
-        self.assertEqual(len(data["details"]), 3)
-        self.assertIsNotNone(data["created_at"])
-        self.assertIsNotNone(data["updated_at"])
+        self.assertEqual(data.pop("id"), self.offer_package_1.id)
+        self.assertEqual(data.pop("user"), self.offer_package_1.user.id)
+        self.assertEqual(data.pop("title"), self.offer_package_1.title)
+        self.assertEqual(
+            data.pop("description"), self.offer_package_1.description
+        )
+        self.assertEqual(data.pop("image"), self.offer_package_1.image)
+        self.assertEqual(data.pop("min_price"), 100)
+        self.assertEqual(data.pop("min_delivery_time"), 5)
+        self.assertEqual(len(data.pop("details")), 3)
+        self.assertIsNotNone(data.pop("created_at"))
+        self.assertIsNotNone(data.pop("updated_at"))
+        self.assertEqual(data, {}, f"Unexpected fields in response: {data}")
 
     def test_offer_retrieve_wrong_id(self):
         url = reverse("offerpackage-detail", kwargs={"pk": 999})
@@ -178,28 +180,42 @@ class TestOfferPackageViewSet(APITestCaseWithSetup):
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         data = response.json()
-        self.assertEqual(data["count"], 2)
-        self.assertEqual(data["next"], None)
-        self.assertEqual(data["previous"], None)
-        result_data = data["results"][0]
-        self.assertEqual(result_data["user"], self.business_user_2.id)
-        self.assertEqual(result_data["title"], "Graphic Design Package")
+        self.assertEqual(data.pop("count"), 2)
+        self.assertEqual(data.pop("next"), None)
+        self.assertEqual(data.pop("previous"), None)
+        result_data = data.pop("results")[0]
+        self.assertEqual(result_data.pop("id"), self.offer_package_2.id)
+        self.assertEqual(result_data.pop("user"), self.business_user_2.id)
+        self.assertEqual(result_data.pop("title"), "Graphic Design Package")
         self.assertEqual(
-            result_data["description"], self.offer_package_2.description
+            result_data.pop("description"), self.offer_package_2.description
         )
-        self.assertEqual(result_data["image"], self.offer_package_2.image)
-        self.assertEqual(result_data["min_price"], 80)
-        self.assertEqual(result_data["min_delivery_time"], 3)
+        self.assertEqual(result_data.pop("image"), self.offer_package_2.image)
+        self.assertEqual(result_data.pop("min_price"), 80)
+        self.assertEqual(result_data.pop("min_delivery_time"), 3)
         self.assertEqual(
-            result_data["user_details"],
+            result_data.pop("details"),
+            [
+                {"id": 4, "url": "/offerdetails/4/"},
+                {"id": 5, "url": "/offerdetails/5/"},
+                {"id": 6, "url": "/offerdetails/6/"},
+            ],
+        )
+
+        self.assertEqual(
+            result_data.pop("user_details"),
             {
                 "first_name": "Sarah",
                 "last_name": "Miller",
                 "username": "sarah_miller",
             },
         )
-        self.assertIsNotNone(result_data["created_at"])
-        self.assertIsNotNone(result_data["updated_at"])
+        self.assertIsNotNone(result_data.pop("created_at"))
+        self.assertIsNotNone(result_data.pop("updated_at"))
+        self.assertEqual(
+            result_data, {}, f"Unexpected fields in response: {result_data}"
+        )
+        self.assertEqual(data, {}, f"Unexpected fields in response: {data}")
 
     def test_offer_list_filter_by_creator_id_1(self):
         self.client.force_authenticate(user=None)
@@ -268,16 +284,16 @@ class TestOfferPackageViewSet(APITestCaseWithSetup):
 
     def test_offer_create_ok(self):
         offer = {
-            "title": "Grafikdesign-Paket",
+            "title": "Graphics Package",
             "image": None,
-            "description": "Ein umfassendes Grafikdesign-Paket für Unternehmen.",
+            "description": "A comprehensive graphics package for businesses.",
             "details": [
                 {
                     "title": "Basic Design",
                     "revisions": 2,
                     "delivery_time_in_days": 5,
                     "price": 100,
-                    "features": ["Logo Design", "Visitenkarte"],
+                    "features": ["Logo Design", "Business Card"],
                     "offer_type": "basic",
                 },
                 {
@@ -285,7 +301,7 @@ class TestOfferPackageViewSet(APITestCaseWithSetup):
                     "revisions": 5,
                     "delivery_time_in_days": 7,
                     "price": 200,
-                    "features": ["Logo Design", "Visitenkarte", "Briefpapier"],
+                    "features": ["Logo Design", "Business Card", "Letterhead"],
                     "offer_type": "standard",
                 },
                 {
@@ -295,8 +311,8 @@ class TestOfferPackageViewSet(APITestCaseWithSetup):
                     "price": 500,
                     "features": [
                         "Logo Design",
-                        "Visitenkarte",
-                        "Briefpapier",
+                        "Business Card",
+                        "Letterhead",
                         "Flyer",
                     ],
                     "offer_type": "premium",
@@ -308,11 +324,8 @@ class TestOfferPackageViewSet(APITestCaseWithSetup):
         response = self.client.post(url, offer, format="json")
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         data = response.json()
-        self.assertEqual(data["title"], "Grafikdesign-Paket")
-        self.assertEqual(
-            data["description"],
-            offer.get("description"),
-        )
+        self.assertEqual(data["title"], "Graphics Package")
+        self.assertEqual(data["description"], offer.get("description"))
         self.assertEqual(len(data["details"]), 3)
         self.assertEqual(OfferPackage.objects.all().count(), 3)
 
@@ -320,16 +333,16 @@ class TestOfferPackageViewSet(APITestCaseWithSetup):
         self.client = TestDataFactory.authenticate_user(self.customer_user_1)
         url = reverse("offerpackage-list")
         offer = {
-            "title": "Grafikdesign-Paket",
+            "title": "Graphics Package",
             "image": None,
-            "description": "Ein umfassendes Grafikdesign-Paket für Unternehmen.",
+            "description": "A comprehensive graphics package for businesses.",
             "details": [
                 {
                     "title": "Basic Design",
                     "revisions": 2,
                     "delivery_time_in_days": 5,
                     "price": 100,
-                    "features": ["Logo Design", "Visitenkarte"],
+                    "features": ["Logo Design", "Business Card"],
                     "offer_type": "basic",
                 },
                 {
@@ -337,7 +350,7 @@ class TestOfferPackageViewSet(APITestCaseWithSetup):
                     "revisions": 5,
                     "delivery_time_in_days": 7,
                     "price": 200,
-                    "features": ["Logo Design", "Visitenkarte", "Briefpapier"],
+                    "features": ["Logo Design", "Business Card", "Letterhead"],
                     "offer_type": "standard",
                 },
                 {
@@ -347,8 +360,8 @@ class TestOfferPackageViewSet(APITestCaseWithSetup):
                     "price": 500,
                     "features": [
                         "Logo Design",
-                        "Visitenkarte",
-                        "Briefpapier",
+                        "Business Card",
+                        "Letterhead",
                         "Flyer",
                     ],
                     "offer_type": "premium",
@@ -363,7 +376,7 @@ class TestOfferPackageViewSet(APITestCaseWithSetup):
             "offerpackage-detail", kwargs={"pk": self.offer_package_1.pk}
         )
         patch_data = {
-            "title": "Updated Grafikdesign-Paket",
+            "title": "Updated Graphics Package",
             "details": [
                 {
                     "title": "Basic Design Updated",
@@ -387,7 +400,7 @@ class TestOfferPackageViewSet(APITestCaseWithSetup):
             "offerpackage-detail", kwargs={"pk": self.offer_package_1.pk}
         )
         patch_data = {
-            "title": "Updated Grafikdesign-Paket",
+            "title": "Updated Graphics Package",
             "details": [
                 {
                     "title": "Basic Design Updated",
@@ -443,7 +456,7 @@ class TestOfferDetailsView(APITestCase):
             revisions=5,
             price=10,
             offer_type="basic",
-            features=["WebDev", "Anderes"],
+            features=["WebDev", "Other"],
             package=cls.offer_package,
         )
 
@@ -455,12 +468,13 @@ class TestOfferDetailsView(APITestCase):
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         data = response.json()
-        self.assertEqual(data["id"], self.offer.id)
-        self.assertEqual(data["title"], self.offer.title)
-        self.assertEqual(data["revisions"], self.offer.revisions)
+        self.assertEqual(data.pop("id"), self.offer.id)
+        self.assertEqual(data.pop("title"), self.offer.title)
+        self.assertEqual(data.pop("revisions"), self.offer.revisions)
         self.assertEqual(
-            data["delivery_time_in_days"], self.offer.delivery_time_in_days
+            data.pop("delivery_time_in_days"), self.offer.delivery_time_in_days
         )
-        self.assertEqual(data["offer_type"], self.offer.offer_type)
-        self.assertEqual(data["price"], self.offer.price)
-        self.assertEqual(data["features"], self.offer.features)
+        self.assertEqual(data.pop("offer_type"), self.offer.offer_type)
+        self.assertEqual(data.pop("price"), self.offer.price)
+        self.assertEqual(data.pop("features"), self.offer.features)
+        self.assertEqual(data, {}, f"Unexpected fields in response: {data}")
