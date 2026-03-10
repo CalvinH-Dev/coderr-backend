1743426371e54a01ad52a01e971e3326aaebc264
CalvinH-Dev <hanisch.calvin@gmail.com>
2026-03-10 22:31:23 +0100

    add: health check to see if the deployed backend is online.

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,15 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.http import JsonResponse
 from django.urls import include, path
 
+
+def health_check(request):
+    return JsonResponse({"status": "ok", "message": "Server is running"})
+
 urlpatterns = [
+    path("", health_check, name="health_check"),
     path("admin/", admin.site.urls),
     path("api/", include("auth_app.api.urls")),
     path("api/", include("offers_app.api.urls")),
